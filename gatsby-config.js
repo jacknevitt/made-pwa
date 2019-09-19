03588fd77105c96dd42c3fbaba3ce6cd4b22dcb1
Jack Nevitt <jack.nevitt@made.com>
2019-09-19 16:53:09 +0100

    metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MADE.COM`,
+    description: `Discover our range of furniture and homeware combining design and affordability. From design sofa to table and lighting create your own space with MADE.com`,
+    author: `MADE.COM`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
