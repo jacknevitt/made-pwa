4f93cce7ab56835bcc7902c09b266e9147b8159d
Jack Nevitt <jack.nevitt@made.com>
2019-09-20 14:00:46 +0100

    styled pdp

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default styled.button`
+  appearance: none;
+  background: black;
+  color: white;
+  font-size: 1em;
+  font-family: inherit;
+  border: 0 none;
+  padding: 10px 15px;
+  text-transform: uppercase;
+`
---
+++ src/components/textLink.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+export default styled(Link)`
+  color: black;
+  text-decoration: none;
+  border-bottom: 1px solid currentColor;
+`
